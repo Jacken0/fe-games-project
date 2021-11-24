2aab61dc2405ce24ef1aadd7359ec9eadfd9fae3
Jacken <mboy@hotmail.co.uk>
2021-11-24 16:53:59 +0000

    adds buttons for voting up and down. Working

--- src/components/VoteReview.js
+++ src/components/VoteReview.js
@@ -3,7 +3,6 @@ import { patchVotes } from "../Utils/api";
 
 export default function VoteReview({ singleReview }) {
   const [addedVote, setAddedVote] = useState(0);
-  console.log(singleReview);
 
   const handleClick = () => {
     setAddedVote((prevVotes) => prevVotes + 1);
@@ -11,8 +10,22 @@ export default function VoteReview({ singleReview }) {
       setAddedVote((prevVotes) => prevVotes - 1);
     });
   };
+  const handleClick2 = () => {
+    setAddedVote((prevVotes) => prevVotes - 1);
+    patchVotes(singleReview.review_id, -1).catch(() => {
+      setAddedVote((prevVotes) => prevVotes + 1);
+    });
+  };
 
   return (
-    <button onClick={handleClick}>{singleReview.votes + addedVote}</button>
+    <>
+      <h4>Votes: {singleReview.votes + addedVote}</h4>
+      <button onClick={handleClick} className="Vote__Button">
+        +
+      </button>
+      <button onClick={handleClick2} className="Vote__Button">
+        -
+      </button>
+    </>
   );
 }
